55c26caa8a2fe8da4be7dcaa1ea581270b81d55e
zj611 <zhangjunbit@163.com>
2022-01-19 22:06:48 +0800

    add twoSum & threeSum

---
+++ nums/three_sum_nums_test.go
@@ -0,0 +1,58 @@
+package nums
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+//给一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，
+//使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+//注意：答案中不可以包含重复的三元组。
+
+func threeSum(nums []int, target int) [][]int {
+	n := len(nums)
+	if n < 3 {
+		return [][]int{}
+	}
+
+	sort.Ints(nums)
+
+	ans := make([][]int, 0)
+
+	for first := 0; first < n; first++ {
+		if first > 0 && nums[first] == nums[first-1] { //当数组索引大于0时，避免重复
+			continue
+		}
+
+		third := n - 1
+		t := target - nums[first]
+
+		for second := first + 1; second < n; second++ {
+			if second > first+1 && nums[second] == nums[second-1] { //当数组索引大于0时，避免重复
+				continue
+			}
+
+			for second < third && nums[second]+nums[third] > t { // 找到一个third就行
+				third--
+			}
+
+			if second == third { //当third减到和second相同时
+				break
+			}
+
+			if nums[second]+nums[third] == t {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
+	}
+	return ans
+}
+
+func TestThreeSum(t *testing.T) {
+	nums := []int{-1, 0, 1, 1, 0, 2}
+
+	ans := threeSum(nums, 2)
+	fmt.Println(ans)
+
+}
---
+++ nums/two_sum_nums_test.go
@@ -0,0 +1,51 @@
+package nums
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+//给一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，
+//使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+//注意：答案中不可以包含重复的三元组。
+
+func twoSum(nums []int, target int) [][]int {
+	n := len(nums)
+	if n < 2 {
+		return [][]int{}
+	}
+
+	sort.Ints(nums)
+
+	ans := make([][]int, 0)
+
+	second := n - 1
+
+	for first := 0; first < n; first++ {
+		if first > 1 && nums[first] == nums[first-1] { //当数组索引大于0时，避免重复
+			continue
+		}
+
+		for first < second && nums[first]+nums[second] > target { // 找到一个second就行
+			second--
+		}
+
+		if first == second { //当second减到和first相同时
+			break
+		}
+
+		if nums[first]+nums[second] == target {
+			ans = append(ans, []int{nums[first], nums[second]})
+		}
+	}
+
+	return ans
+}
+
+func TestTwoSum(t *testing.T) {
+	nums := []int{-1, 0, 1, 1, 0, 2}
+
+	ans := twoSum(nums, 1)
+	fmt.Println(ans)
+}
